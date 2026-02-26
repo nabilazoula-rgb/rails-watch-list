9cca3d245d171cfa16fd896eee82962c19eb5a1f
nabilazoula <nabil.azoula@gmail.com>
2026-02-26 16:47:22 +0100

    Watch List SCSS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_26_112455) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_26_150504) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_26_112455) do
 
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "image_url"
     t.text "overview"
     t.string "poster_url"
     t.float "rating"
